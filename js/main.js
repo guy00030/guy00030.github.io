87b1ca6c82233f96cf0bba57a73a861e326b37d6
guy00030 <guy00030@algonquinlive.com>
2014-11-11 10:18:39 -0500

    removed sun

--- js/main.js
+++ js/main.js
@@ -7,9 +7,6 @@ $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 
 	/* console.log(scrollPos); < for debugging*/
-
-	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
-	$sunSection.css('background-position', 'center ' + scrollPos / 3 + 'px');
 });
 
 /**
